d2a03703b58db5f783935c5321c08487b3814c50
TreyCarterSmith <smittrey@mail.gvsu.edu>
2023-01-29 15:04:00 -0500

    material calendar

--- app/src/main/java/com/treybuchet/sharkradar/MainActivity.kt
+++ app/src/main/java/com/treybuchet/sharkradar/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.treybuchet.sharkradar
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.treybuchet.sharkradar.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -17,6 +18,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, CalendarActivity::class.java)
             startActivity(intent)
         }
+
+    }
+
+    fun changeColorDate(date: Date) {
 
     }
 }
